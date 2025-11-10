4da0a6d4fd247f8af415e1cf3ed407b8242605c7
yishay ossi <yishayossi10@gmail.com>
2025-11-10 18:00:15 +0200

    crate library

--- book.py
+++ book.py
@@ -1,10 +1,16 @@
 import uuid
+
 class Book:
-    def __init__(self,title:str,author:str):
+    def __init__(self, title: str, author: str):
         self.title = title
         self.author = author
-        self.isbn = uuid.uuid4()
+        self.isbn = str(uuid.uuid4())[:4]
         self.is_available = True
+
     def __str__(self):
-        print( str(self.__dict__))
+        return f"title: {self.title} author: {self.author} isbn: {self.isbn} is_available: {self.is_available}"
+
+    def __repr__(self):
+        return self.__str__()
+
     
--- user.py
+++ user.py
@@ -1,9 +1,14 @@
 class User:
-    def __init__(self, name:str, id:int, borrowed_books:list):
+    def __init__(self, name:str, id:int):
         self.name = name
         self.id = id
-        self.borrowed_books = borrowed_books
+        self.borrowed_books = []
+        
     def __str__(self):
-        return str(self.__dict__)
+        return f"name: {self.name} id: {self.id} borrowed_books: {self.borrowed_books}"
+
+    def __repr__(self):
+        return self.__str__()
+
 
 
